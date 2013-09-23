889ffefe06700a85baae830908cc6ff6bcee88fe
Benjamin Attal <attal.benjamin@gmail.com>
2013-09-23 18:50:30 -0400

    Update app.py with url for main service

--- 7/attal_benjamin/app.py
+++ 7/attal_benjamin/app.py
@@ -5,10 +5,12 @@ from flask import Flask, render_template, request
 app = Flask(__name__)
 
 @app.route('/')
-@app.route('/index.html')
 def index():
   return render_template('index.html')
 
+@app.route('/pvwatts.html', methods=['GET', 'POST'])
+def pvwatts():
+  return render_template('pvwatts.html')
 
 if __name__ == '__main__':
   app.run(debug=True)
