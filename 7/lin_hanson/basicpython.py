63e64bf963cf8fb7a3e2789412135088f55517b1
Hanson Lin <hanson@hanson-VirtualBox.(none)>
2013-09-23 20:34:53 -0400

    Added basic python file

---
+++ 7/lin_hanson/basicpython.py
@@ -0,0 +1,11 @@
+
+def pythonknowledge():
+    return "I'm still very lacking in python ability :("
+
+def numberslessthan():
+    n = raw_input("Enter a low number:")
+    for i in range(1,int(n)+1):
+        print(i)
+
+print pythonknowledge()
+print numberslessthan()
